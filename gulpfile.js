10c9c91210f609f2548f0b1fc33068dcac260181
paperbits <operations@paperbits.io>
2016-08-13 22:58:14 -0700

    added script for collecting analytics

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require("gulp-autoprefixer");
 var bower = require("main-bower-files");
 var concat = require("gulp-concat");
 var webserver = require("gulp-webserver");
+var typescript = require("typescript");
+var typescriptCompiler = require("gulp-typescript");
 var uglify = require("gulp-uglify");
 
 function handleErrors(error) {
-    console.log('ERROR');
+    console.log("ERROR");
     console.log(error.toString());
     this.emit("end");
 }
@@ -17,7 +19,7 @@ gulp.task("serve", ["build", "watch"], function () {
         .pipe(webserver({
             port: "80",
             livereload: true,
-            fallback: 'index.html',
+            fallback: "index.html",
             open: false
         }));
 });
@@ -29,7 +31,7 @@ gulp.task("theme", function () {
         "src/images/**/*.*",
         "src/*.html",
         "src/**/*.json"],
-        { base: 'src/' })
+        { base: "src/" })
         .pipe(gulp.dest("public"));
 });
 
@@ -42,10 +44,26 @@ gulp.task("theme-styles", function () {
         .pipe(gulp.dest("public/styles"));
 });
 
-gulp.task("build", ["theme", "theme-styles"]);
+gulp.task("theme-scripts", function () {
+    var tsProject = typescriptCompiler.createProject("tsconfig.json", {
+        sortOutput: true,
+        typescript: typescript
+    });
+
+    var tsResult = tsProject
+        .src()
+        .pipe(typescriptCompiler(tsProject));
+
+    return tsResult.js
+        .pipe(concat("paperbits-demo.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("public/scripts"));
+});
+
+gulp.task("build", ["theme", "theme-styles", "theme-scripts"]);
 
 gulp.task("watch", function () {
-    gulp.watch(["src/**/*.*"], ["theme", "theme-styles"]).on("error", handleErrors);
+    gulp.watch(["src/**/*.*"], ["theme", "theme-styles", "theme-scripts"]).on("error", handleErrors);
 });
 
 gulp.task("default", ["build", "watch"]);
