8f1c86357e2e310f610ec393e273d5aaa688130e
azaslonov <zaslonov.alexander@gmail.com>
2018-03-31 18:31:58 -0700

    minor adjustments for production build

--- webpack.build.js
+++ webpack.build.js
@@ -1,13 +1,9 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
     plugins: [
-        new webpack.DefinePlugin({	
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
         new UglifyJSPlugin({
             uglifyOptions: {
                 ie8: false,
