080a98aadd9761a7c7b6fc0b24551915fdf05d9f
azaslonov <zaslonov.alexander@gmail.com>
2019-02-02 18:06:20 -0800

    cleaning up startup files

--- src/startup.publish.ts
+++ src/startup.publish.ts
@@ -6,7 +6,6 @@
  * found in the LICENSE file and at https://paperbits.io/license.
  */
 
-import * as ko from "knockout";
 import { InversifyInjector } from "@paperbits/common/injection";
 import { IPublisher } from "@paperbits/common/publishing";
 import { PublishingNodeModule } from "./publishing";
@@ -16,29 +15,34 @@ import { CoreModule } from "@paperbits/core/core.module";
 import { StyleModule } from "@paperbits/styles/styles.module";
 import { ProseMirrorModule } from "@paperbits/prosemirror/prosemirror.module";
 import { DemoModule } from "./components/demo.module";
+
+/* Uncomment to enable Firebase module */
 // import { FirebaseModule } from "@paperbits/firebase/firebase.admin.module";
 
-const outputBasePath = "./dist/website";
-const settingsPath = "./dist/publisher/config.json";
-const dataPath = "./dist/publisher/data/demo.json";
-
+/* Initializing dependency injection container */
 const injector = new InversifyInjector();
 injector.bindModule(new CoreModule());
 injector.bindModule(new FormsModule());
 injector.bindModule(new EmailsModule());
 injector.bindModule(new StyleModule());
 injector.bindModule(new ProseMirrorModule());
+
+/* Initializing Demo module */
+const outputBasePath = "./dist/website";
+const settingsPath = "./dist/publisher/config.json";
+const dataPath = "./dist/publisher/data/demo.json";
 injector.bindModule(new DemoModule(dataPath, settingsPath, outputBasePath));
+
+/* Uncomment to enable Firebase module */
 // injector.bindModule(new FirebaseModule());
 
 injector.bindModule(new PublishingNodeModule());
 injector.resolve("autostart");
 
-ko.options["createChildContextWithAs"] = true;
-ko.applyBindings();
-
+/* Bulding dependency injection container */
 const publisher = injector.resolve<IPublisher>("sitePublisher");
 
+/* Running actual publishing */
 publisher.publish()
     .then(() => {
         console.log("DONE.");
