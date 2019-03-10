b8b412987ffca872c917b180f13374e470efc964
azaslonov <zaslonov.alexander@gmail.com>
2019-03-09 20:57:21 -0800

    fixed license URL

--- src/components/staticSettingsProvider.ts
+++ src/components/staticSettingsProvider.ts
@@ -3,7 +3,7 @@
  * Copyright Paperbits. All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file and at style-guidehttps://paperbits.io/license/mit.
+ * found in the LICENSE file and at https://paperbits.io/license/mit.
  */
 
 import { ISettingsProvider } from "@paperbits/common/configuration/ISettingsProvider";
