8bc72fc5130ebd3f763226264d64c9e0929d9ed2
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-01-11 11:56:58 -0800

    Added full-text search functionality. Upgraded paperbits libraries to 0.1.203.

--- src/components/demo.runtime.module.ts
+++ src/components/demo.runtime.module.ts
@@ -6,31 +6,32 @@
  * found in the LICENSE file and at https://paperbits.io/license/mit.
  */
 
-import { IInjector, IInjectorModule } from "@paperbits/common/injection";
-import { DefaultEventManager } from "@paperbits/common/events";
-import { DefaultRouter, DefaultRouteGuard } from "@paperbits/common/routing";
-import { VisibilityGuard } from "@paperbits/common/user";
-import { KnockoutRegistrationLoaders } from "@paperbits/core/ko/knockout.loaders";
-import { SearchResultOutput } from "../themes/website/scripts/searchResultsOutput";
 import "@paperbits/core/ko/bindingHandlers/bindingHandlers.component";
 import "@paperbits/core/collapsible-panel/runtime/bindingHandlers.toggleCollapsible";
+import { IInjector, IInjectorModule } from "@paperbits/common/injection";
+import { DefaultEventManager } from "@paperbits/common/events";
+import { DefaultRouter } from "@paperbits/common/routing";
+import { VisibilityGuard } from "@paperbits/common/user";
+import { XmlHttpRequestClient } from "@paperbits/common/http";
+import { KnockoutRegistrationLoaders } from "@paperbits/core/ko/knockout.loaders";
 import { CollapseToggle } from "@paperbits/core/collapsible-panel/collapseToggle";
 import { StaticUserService } from "./staticUserService";
 import { StaticRoleService } from "./staticRoleService";
+import { SearchRuntimeModule } from "@paperbits/core/search/search.runtime.module";
 import { ClickCounterRuntimeModule } from "./click-counter/ko/runtime/clickCounter.runtime.module";
-// import { ClickCounterRuntimeModule } from "./click-counter/vue/runtime/clickCounter.runtime.module";
 
 
 export class DemoRuntimeModule implements IInjectorModule {
     public register(injector: IInjector): void {
         injector.bindModule(new KnockoutRegistrationLoaders());
         injector.bindModule(new ClickCounterRuntimeModule());
+        injector.bindModule(new SearchRuntimeModule());
         injector.bindSingleton("eventManager", DefaultEventManager);
         injector.bindCollection("autostart");
         injector.bindCollection("routeGuards");
         injector.bindSingleton("router", DefaultRouter);
-        injector.bind("searchResultOutput", SearchResultOutput);
         injector.bind("collapseToggle", CollapseToggle);
+        injector.bind("httpClient", XmlHttpRequestClient);
         injector.bindToCollection("autostart", VisibilityGuard);
         injector.bindSingleton("userService", StaticUserService);
         injector.bindSingleton("roleService", StaticRoleService);
