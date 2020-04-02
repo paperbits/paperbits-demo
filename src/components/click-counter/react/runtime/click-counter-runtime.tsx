48007a3eef0cf942b78d1b7f18d349407f51edd0
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-04-01 19:25:28 -0700

    Added return type.

--- src/components/click-counter/react/runtime/click-counter-runtime.tsx
+++ src/components/click-counter/react/runtime/click-counter-runtime.tsx
@@ -22,12 +22,12 @@ export class ClickCounterRuntime extends React.Component {
         this.setState({ clickCount: this.state.clickCount + 1 });
     }
 
-    public render() {
+    public render(): JSX.Element {
         return (
             <div>
                 <fieldset className="text text-align-center">
                     <button className="button" onClick={this.increaseCount}>
-                        Click me (React)
+                        Click me
                     </button>
                     <div>
                         <label htmlFor="clickCount">Click count:</label>
