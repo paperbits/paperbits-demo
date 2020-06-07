278aca338d8607de405c372245b923d5e1b39c20
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-06-07 13:11:24 -0700

    Added basic Angular runtime component example.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,9 @@
-import "@webcomponents/custom-elements";
+import "@webcomponents/custom-elements/src/native-shim";
 import "core-js/es/array";
 import "core-js/es/object";
 import "core-js/es/promise";
 import "core-js/es/reflect";
 import "core-js/es/symbol";
+
+/* Angular dependency */
+import "zone.js/dist/zone";
