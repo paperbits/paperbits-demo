ae24d13c43aafd8e96b4a2b3d1532ab89c919c65
Tuan Nguyen <atuannguyen1101@gmail.com>
2020-06-28 15:20:11 -0400

    Browser noti

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -10,6 +10,8 @@ import template from "./app.html";
 import { ViewManager } from "@paperbits/common/ui";
 import { Component, OnMounted } from "@paperbits/common/ko/decorators";
 import { Logger } from "@paperbits/common/logging";
+import { detect } from "detect-browser"
+import { versionCompare } from "../utils";
 
 @Component({
     selector: "app",
@@ -23,8 +25,36 @@ export class App {
 
     @OnMounted()
     public async initialize(): Promise<void> {
+        const browser = detect();
+        switch(browser.name) {
+            case "chrome": {
+                versionCompare(browser.version, "83.0.4103.116", null) > -1 ? this.supportedBrowser() : this.notSupportedBrowser();
+                break;
+            }
+            case "safari": {
+                versionCompare(browser.version, "13.1.1", null) > -1 ? this.supportedBrowser() : this.notSupportedBrowser();
+                break;
+            }
+            case "firefox": {
+                versionCompare(browser.version, "77.0.1", null) > -1 ? this.supportedBrowser() : this.notSupportedBrowser();
+                break;
+            }
+            default: {
+                browser.name.includes("edge") ? this.supportedBrowser() : this.notSupportedBrowser();
+                break;
+            }
+        }
+    }
+
+    private async supportedBrowser(): Promise<void> {
         this.viewManager.setHost({ name: "page-host" });
         this.viewManager.showToolboxes();
+        this.logger.traceEvent("App started.");
+    }
+
+    private async notSupportedBrowser(): Promise<void> {
+        this.viewManager.setHost({ name: "invalid-browser" });
+        this.viewManager.removeShutter();
         this.logger.traceEvent("App started.");
     }
 }
