4551d9702002e742fa33227cb917722621aa2140
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-07-14 13:10:37 -0700

    Adjusted logging interface.

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -25,6 +25,6 @@ export class App {
     public async initialize(): Promise<void> {
         this.viewManager.setHost({ name: "page-host" });
         this.viewManager.showToolboxes();
-        this.logger.trackEvent("App started.");
+        this.logger.trackEvent("Startup", { message: `App started.` });
     }
 }
