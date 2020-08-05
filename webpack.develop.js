d5c831775ded2a3e1461cef1957c82dc124f44be
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-08-04 19:04:49 -0700

    Upgraded paperbits libraries to 0.1.307. Upgraded dependencies.

--- webpack.develop.js
+++ webpack.develop.js
@@ -1,5 +1,5 @@
 const webpack = require("webpack");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const designerConfig = require("./webpack.designer.js");
 
