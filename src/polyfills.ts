c7921d7bb2b5ccb7e3541c7536fad173759b613c
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-11-07 20:06:42 -0800

    Upgraded paperbits libraries to 0.1.349.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,9 +1,6 @@
-import "@webcomponents/custom-elements/src/native-shim";
+import "@webcomponents/custom-elements";
 import "core-js/es/array";
 import "core-js/es/object";
 import "core-js/es/promise";
 import "core-js/es/reflect";
 import "core-js/es/symbol";
-
-/* Angular dependency */
-import "zone.js/dist/zone";
