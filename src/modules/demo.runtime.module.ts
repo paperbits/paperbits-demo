4c8020f16f85f8220a21b950926b08b9bed3573b
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-05-15 16:08:10 -0700

    Refactoring module registrations.

--- src/modules/demo.runtime.module.ts
+++ src/modules/demo.runtime.module.ts
@@ -8,12 +8,6 @@
 
 import "@paperbits/core/ko/bindingHandlers/bindingHandlers.component";
 import { IInjector, IInjectorModule } from "@paperbits/common/injection";
-import { DefaultEventManager } from "@paperbits/common/events";
-import { CoreRuntimeModule } from "@paperbits/core/core.runtime.module";
-import { DefaultRouter, HistoryRouteHandler, LocationRouteHandler } from "@paperbits/common/routing";
-import { VisibilityGuard } from "@paperbits/common/user";
-import { XmlHttpRequestClient } from "@paperbits/common/http";
-import { KnockoutRegistrationLoaders } from "@paperbits/core/ko/knockout.loaders";
 import { StyleRuntimeModule } from "@paperbits/styles/styles.runtime.module";
 import { StaticUserService } from "../user/staticUserService";
 import { StaticRoleService } from "../user/staticRoleService";
@@ -22,19 +16,9 @@ import { ClickCounterRuntimeModule } from "../components/click-counter/clickCoun
 
 export class DemoRuntimeModule implements IInjectorModule {
     public register(injector: IInjector): void {
-        injector.bindModule(new KnockoutRegistrationLoaders());
         injector.bindModule(new ClickCounterRuntimeModule());
         injector.bindModule(new StyleRuntimeModule());
-        injector.bindSingleton("eventManager", DefaultEventManager);
-        injector.bindCollection("autostart");
-        injector.bindCollection("routeGuards");
-        injector.bindSingleton("router", DefaultRouter);
-        injector.bind("httpClient", XmlHttpRequestClient);
-        injector.bindToCollection("autostart", VisibilityGuard);
         injector.bindSingleton("userService", StaticUserService);
         injector.bindSingleton("roleService", StaticRoleService);
-        injector.bindToCollection("autostart", location.href.includes("designtime=true")
-            ? HistoryRouteHandler
-            : LocationRouteHandler);
     }
 }
