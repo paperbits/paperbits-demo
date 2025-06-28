278adb387132783e21a2ce27e029a72a433bb91f
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2025-06-28 10:19:15 -0700

    Fixed toolboxes styling.

--- src/persistence/httpDataProvider.ts
+++ src/persistence/httpDataProvider.ts
@@ -6,36 +6,37 @@
  * found in the LICENSE file and at https://paperbits.io/license/mit.
  */
 
- import { HttpClient } from "@paperbits/common/http";
+import { HttpClient } from "@paperbits/common/http";
 
- export class HttpDataProvider {
-     private dataObject: Object;
-     private dataObjectPromise: Promise<Object>;
- 
-     constructor(private readonly httpClient: HttpClient) { }
- 
-     private async loadData(): Promise<Object> {
-         const response = await this.httpClient.send({
-             url: "/data/demo.json",
-             method: "GET"
-         });
- 
-         this.dataObject = response.toObject();
- 
-         return this.dataObject;
-     }
- 
-     public async getDataObject(): Promise<Object> {
-         if (this.dataObjectPromise) {
-             return this.dataObjectPromise;
-         }
- 
-         this.dataObjectPromise = this.loadData();
- 
-         return this.dataObjectPromise;
-     }
- 
-     public async setDataObject(dataObject: Object): Promise<void> {
-         this.dataObject = dataObject;
-     }
- }
+export class HttpDataProvider {
+    private dataObject: Object;
+    private dataObjectPromise: Promise<Object>;
+
+    constructor(private readonly httpClient: HttpClient) { }
+
+    private async loadData(): Promise<Object> {
+        const response = await this.httpClient.send({
+            url: "/data/demo.json",
+            method: "GET"
+        });
+
+        this.dataObject = response.toObject();
+
+        return this.dataObject;
+    }
+
+    public async getDataObject(): Promise<Object> {
+        if (this.dataObjectPromise) {
+            return this.dataObjectPromise;
+        }
+
+        this.dataObjectPromise = this.loadData();
+
+        return this.dataObjectPromise;
+    }
+
+    public async setDataObject(dataObject: Object): Promise<void> {
+        this.dataObjectPromise = Promise.resolve(dataObject);
+        this.dataObject = dataObject;
+    }
+}
